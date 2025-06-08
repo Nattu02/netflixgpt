2e8a26eba67dfa9a67e773f224487870c12bae53
Nattudurai <nattuduraic.02@gmail.com>
2025-06-08 11:44:13 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='font-bold text-center mt-10 text-2xl'>
+        Hello world.. This is the home page..
+        </div>
+    </>
+  )
+}
+
+export default App
